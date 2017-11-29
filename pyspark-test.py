7d0b9152fda7715bd3641f100efbf5b9146c5bb2
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-11-29 15:26:13 -0600

    adds comments

--- pyspark-test.py
+++ pyspark-test.py
@@ -1,17 +1,26 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
+# Spark configuration
 conf = (SparkConf()
     .setMaster("local")
     .setAppName("My app")
     .set("spark.executor.memory", "1g"))
 
+# setting Spark contextas the config we set
 sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
 
+# constructing a dataframe from a CSV
 df = sqlContext.read.load('./test.csv',
+    # format of the data
     format='com.databricks.spark.csv',
+    # means that the csv files contains the header
     header= 'true',
+    # telling sqlContext to automatically detect the data type of each column in data frame. 
+    # If we do not set inferSchema to be true, all columns will be read as string
     inferSchema='true')
 
+print(type(df))
+# show dataframe in the console
 df.show()
